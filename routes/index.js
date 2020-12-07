37abfbf051eca862b6aaf67cf4f9c8c7ad3b8ca9
Caterina Natali <caterinanatali12@gmail.com>
2020-12-07 19:34:15 +0100

    fixing errors

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-//Chiave di Firebase
+//Firebase Admin SDK
 let admin = require("firebase-admin");
 
 let serviceAccount = require("./../serviceAccount.json");
@@ -7,37 +7,44 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+//express module
 const express = require("express");
 const router = express.Router();
+
+//remote db
 const db = admin.firestore();
+
+//local db
 const quotes =[];
 
 
-//FEATURE POST
 
-//generatore di id univoci
+
+//Univoque id generator
 function genId() {  
 return quotes.length > 0 ? Math.max(...quotes.map(quote => quote.id)) + 1 : 1
 }
 
+//POST /quotes
 router.post("/quotes", async(req,res) =>{
     try{
         let list = await db.collection('quotes').get();
         list.forEach(doc => quotes.push(doc.data()));
         if(!req.body.author || !req.body.quote){
-            return res.status(404).json ({message: "insert quote or author!"});
+            return res.status(400).json ({message: "Bad request: missing quote or author in the request body!"});
         }
         const newId = genId();
-        let q = {
+        let newQuote = {
             id: newId,
             quote: req.body.quote,
             author: req.body.author
         }
-        db.collection('quotes').doc(newId.toString()).set(q);
+        db.collection('quotes').doc(newId.toString()).set(newQuote);
         return res.status(201).json({ message: "Created" });
     } catch (error) {
-        return res.status(500).send(error);
+        return res.status(500).send({error:error.toString()});
     }
 });
 
+//export module
 module.exports = router;
