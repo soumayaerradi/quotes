7cfb99eaa6170c9b7126287bd98f7e10babef83d
Gio <gv.mombelli@gmail.com>
2020-12-07 21:29:18 +0100

    features patch

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,6 @@ function genId(quotes) {
     return quotes.length > 0 ? Math.max(...quotes.map(quote => quote.id)) + 1 : 1;
 }
 
-// PATCH
 router.patch("/quotes/:id", async (req,res) => {
     try {
         quotes.length = 0;
@@ -25,11 +24,14 @@ router.patch("/quotes/:id", async (req,res) => {
             return res.status(400).json({message: "You have to pass a quote and/or an author :/"});
         } 
         const q = await db.collection("quotes").doc(req.params.id).get();
-        console.log(q);
         if (!q.data()){
             return res.status(404).json({message: "Quote not found :("});
         }
-        
+
+        if (req.body.quote && req.body.author){
+            db.collection("quotes").doc(req.params.id).set({quote: req.body.quote, author: req.body.author}, {merge: true});
+            return res.json({message: "Updated"});
+        }
         if (req.body.quote){
             db.collection("quotes").doc(req.params.id).set({quote: req.body.quote}, {merge: true});
             return res.json({message: "Updated"});
@@ -39,15 +41,6 @@ router.patch("/quotes/:id", async (req,res) => {
             return res.json({message: "Updated"});
         }
         
-        // const quote = quotes.find(valore => valore.id === Number(req.params.id));
-        // if (req.body.quote){
-        //     quote.quote = req.body.quote;
-        //     return res.json({message: "Updated"});
-        // }
-        // if (req.body.author) {
-        //     quote.author = req.body.author;
-        //     return res.json({message: "Updated"});
-        // }
     } catch (error) {
         return res.status(500).send(error);
     }
